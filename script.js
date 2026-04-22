3558b5e75da18c50e338a9e37f5d1fc23b4fb357
Beatriz Gabriele Silva Teles <beatrizgabrielesilvateles@gmail.com>
2026-04-22 15:35:06 -0300

    Script inicial adicionado

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Projeto iniciado!");
+
+// Função do botão
+function mostrarMensagem() {
+    alert("Obrigado por visitar meu portfólio!");
+}
+
+// Lista dinâmica (extra)
+let projetos = [
+    "Site Livraria",
+    "Sistema de notas escolar",
+    "Sistema de boletim",
+    "Loja de roupas online"
+];
+
+for (let i = 0; i < projetos.length; i++) {
+    console.log("Projeto: " + projetos[i]);
+}
+
+// Após conflito (etapa final)
+console.log("Conflito resolvido com sucesso!");
